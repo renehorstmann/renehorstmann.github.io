cc854e6f2661715f434e72afbcf87571b44c2e56
renehorstmann <r.horsty@gmail.com>
2022-06-10 17:20:43 +0200

    tea

--- tea/res/r/particle.glsl
+++ tea/res/r/particle.glsl
@@ -14,7 +14,7 @@
     layout(location = 12) in vec4 in_axis_angle;
     layout(location = 13) in vec4 in_color_speed;
 
-    layout(location = 14) in float in_start_time;
+    layout(location = 14) in float in_delta_time;
 
     // on some Browsers (Chrome on Desktop?!?) location = 15 is not valid, even if GL_MAX_VERTEX_ATTRIBS == 16
 
@@ -23,7 +23,6 @@
 
     uniform mat4 vp;
     uniform vec2 sprites;
-    uniform float time;
 
     const vec4 vertices[6] = vec4[](
     vec4(-0.5, -0.5, 0, 1),
@@ -68,7 +67,7 @@
         vec2 in_sprite_speed = in_sprite_and_sprite_speed.zw;
         
         
-        float dt = time - in_start_time;
+        float dt = in_delta_time;
 
         vec4 pos = vertices[gl_VertexID];
 
@@ -106,66 +105,9 @@
 
     out vec4 out_frag_color;
 
-    uniform float scale; // aka real_pixel_per_pixel
-    // in texture space (origin is top left) [0:1]
-    // as center_x, _y, radius_x, _y
-    // example for fullscreen: 0.5, 0.5, 0.5, 0.5
-    uniform vec4 view_aabb;
-
-
-    uniform sampler2DArray tex_main;
-    uniform sampler2DArray tex_refraction;
-    uniform sampler2D tex_framebuffer;
-
-
-    // returns the distance to the border of space
-    // space as aabb in center_x, center_y, radius_x, radius_y
-    float space_distance(vec2 pos, vec4 space) {
-        vec2 center = space.xy;
-        vec2 radius = space.zw;
-        vec2 dist = max(vec2(0, 0), radius - abs(pos-center));
-        return min(dist.x, dist.y);
-    }
-
+    uniform sampler2DArray tex;
 
     void main() {
-        vec2 tex_refraction_size = vec2(textureSize(tex_refraction, 0));
-        vec2 tex_framebuffer_size = vec2(textureSize(tex_framebuffer, 0));
-
-        vec4 refract = texture(tex_refraction, v_tex_coord);
-
-        // refract.z = stretch_val_x + stretch_val_y*16
-        // stretch_val == 12.0f -> normal (8+4)
-        // stretch_val == 4.0f -> mirror  (8-4)
-        vec2 stretch = vec2(
-        mod(refract.z*255.0f,16.0f)-8.0f,
-        floor(refract.z*255.0f/16.0f)-8.0f
-        );
-        stretch.x = stretch.x / 4.0f - 1.0f;
-        stretch.y = 1.0f - stretch.y / 4.0f;
-
-        // framebuffer offset in real pixel coords
-        vec2 offset = (refract.xy - 0.5f) * 255.0f;
-        offset = offset + stretch * (v_tex_coord.xy - 0.5f) * tex_refraction_size;
-        offset = offset * scale;  // intern pixel -> real pixel
-
-
-        // grab coords for framebuffer
-        vec2 r_coord;
-        r_coord.x = (gl_FragCoord.x + offset.x) / tex_framebuffer_size.x;
-        r_coord.y = 1.0f - (gl_FragCoord.y + offset.y) / tex_framebuffer_size.y;
-
-        // if the coords are near the view space, or out of it, mix alpha to 0
-        float alpha = mix(refract.a, 0.0f,
-        max(0.0f, 1.0f-5.0f*space_distance(r_coord, view_aabb)));
-
-        // blend main and refraction
-        vec4 color = texture(tex_main, v_tex_coord);
-        color.rgb = mix(color.rgb,
-        texture(tex_framebuffer, r_coord).rgb, alpha);
-
-        // add global color
-        out_frag_color = color * v_color;
+        out_frag_color = texture(tex, v_tex_coord) * v_color;
     }
-
 #endif
